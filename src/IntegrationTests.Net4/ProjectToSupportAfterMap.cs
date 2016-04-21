ac7267e4d8375ca078f6ce4002757478b6e44016
Brett Veenstra <brettveenstra@gmail.com>
2016-04-21 07:09:42 -0400

    start test scenario

---
+++ src/IntegrationTests.Net4/ProjectToSupportAfterMap.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using AutoMapper.QueryableExtensions;
+using AutoMapper.UnitTests;
+using Should;
+using Xunit;
+
+namespace AutoMapper.IntegrationTests.Net4
+{
+  public class ProjectToSupportAfterMap : AutoMapperSpecBase
+  {
+    private ConcreteTypeA[] _destinations;
+
+    public class ConcreteTypeA
+    {
+      public string FirstName { get; set; }
+      public int ID { get; set; }
+      public string Name { get; set; }
+    }
+
+    public class DbEntityA
+    {
+      public int ID { get; set; }
+      public string Name { get; set; }
+    }
+
+    public class DatabaseInitializer : CreateDatabaseIfNotExists<Context>
+    {
+      protected override void Seed(Context context)
+      {
+        context.EntityA.AddRange(new[]
+        {
+          new DbEntityA {ID = 1, Name = "Alain Brito"},
+          new DbEntityA {ID = 2, Name = "Jimmy Bogard"},
+          new DbEntityA {ID = 3, Name = "Bill Gates"}
+        });
+        base.Seed(context);
+      }
+    }
+
+    public class Context : DbContext
+    {
+      public Context()
+      {
+        Database.SetInitializer(new DatabaseInitializer());
+      }
+
+      public DbSet<DbEntityA> EntityA { get; set; }
+    }
+
+    protected override MapperConfiguration Configuration => new MapperConfiguration(cfg =>
+    {
+      cfg.CreateMap<DbEntityA, ConcreteTypeA>()
+        .AfterMap((d, c) => { c.FirstName = c.Name.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)[0]; });
+    });
+
+    [Fact]
+    public void Should_project_and_execute_AfterMap()
+    {
+      using (var context = new Context())
+      {
+        _destinations = context.EntityA.ProjectTo<ConcreteTypeA>(Configuration).ToArray();
+      }
+      _destinations.Length.ShouldEqual(3);
+      _destinations[2].Name.ShouldEqual("Bill Gates");
+      _destinations[2].FirstName.ShouldEqual("Bill");
+      _destinations[1].FirstName.ShouldEqual("Jimmy");
+      _destinations[0].FirstName.ShouldEqual("Alain");
+    }
+  }
+}
